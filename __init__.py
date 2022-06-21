ea60d17e6c087dd01d5824e875312dc9ca43586b
Steven Xu <stevenxxiu@gmail.com>
2022-06-21 18:01:07 +1000

    feat: add `All` option to copy all search results

--- __init__.py
+++ __init__.py
@@ -33,8 +33,11 @@ def handleQuery(query):
         if e.returncode == 1 and e.output == 'uni: no matches\n':
             return None
         raise
+    entries = json.loads(output)
+    for entry in entries:
+        entry['utf8'] = '\\x' + '\\x'.join(entry['utf8'].split(' '))
     items = []
-    for entry in json.loads(output):
+    for entry in entries:
         items.append(
             Item(
                 id=__title__,
@@ -45,9 +48,38 @@ def handleQuery(query):
                     ClipAction(text='Copy Char', clipboardText=entry['char']),
                     ClipAction(text='Copy JSON', clipboardText=entry['json']),
                     ClipAction(text='Copy HTML', clipboardText=entry['html']),
-                    ClipAction(text='Copy UTF-8 bytes', clipboardText='\\x' + '\\x'.join(entry['utf8'].split(' '))),
+                    ClipAction(text='Copy UTF-8 bytes', clipboardText=entry['utf8']),
                     ClipAction(text='Copy All', clipboardText=json.dumps(entry, indent=4, sort_keys=True)),
                 ],
             )
         )
+
+    items.append(
+        Item(
+            id=__title__,
+            icon=ICON_PATH,
+            text='All',
+            actions=[
+                ClipAction(text='Copy Char', clipboardText='\n'.join(entry['char'] for entry in entries)),
+                ClipAction(
+                    text='Copy JSON',
+                    clipboardText='\n'.join(f'{entry["char"]} {entry["json"]}' for entry in entries),
+                ),
+                ClipAction(
+                    text='Copy HTML',
+                    clipboardText='\n'.join(f'{entry["char"]} {entry["html"]}' for entry in entries),
+                ),
+                ClipAction(
+                    text='Copy UTF-8 bytes',
+                    clipboardText='\n'.join(f'{entry["char"]} {entry["utf8"]}' for entry in entries),
+                ),
+                ClipAction(
+                    text='Copy All',
+                    clipboardText='\n'.join(
+                        f'{entry["char"]} {json.dumps(entry, indent=4, sort_keys=True)}' for entry in entries
+                    ),
+                ),
+            ],
+        )
+    )
     return items
